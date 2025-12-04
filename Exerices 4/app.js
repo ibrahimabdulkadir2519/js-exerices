b0247f9a2b08cd19a5c9a057bc4b71cacdaa7a4f
ibrahimabdulkadir2519 <ibrahimabdulkadir2519@gmail.com>
2025-12-05 07:48:52 +1300

    sumbit Exerices 3

---
+++ Exerices 4/app.js
@@ -0,0 +1,17 @@
+// function declaration
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(3, 4));   
+console.log(add(10, 20)); 
+console.log(add(1, 9));   
+
+// function expression
+const multiply = function(a, b) {
+  return a * b;
+};
+
+console.log(multiply(3, 4));   
+console.log(multiply(10, 20)); 
+console.log(multiply(1, 9));
