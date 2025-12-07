a3124312c00169e3433b95ad111aae0ededf6ac8
ibrahimabdulkadir2519 <ibrahimabdulkadir2519@gmail.com>
2025-12-08 05:26:32 +1300

    sumbit Exerices 10

---
+++ Exerices 10/app.js
@@ -0,0 +1,19 @@
+
+
+// object arrays
+
+let libarary =[
+      {
+        title: "The Road Ahead",
+        auther: "Bill Gates",
+        year: 1995
+      },
+      {
+       title: "Walter Isaacson",
+       auther: "Steve Jobs",
+       year: 2011
+      }
+]
+
+console.log(libarary[0].title);
+console.log(libarary[1].auther);
